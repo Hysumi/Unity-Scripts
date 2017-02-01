4f85852801d0f59572443c87970e32c1f86e4e5e
Hysumi <hysumi@gmail.com>
2017-02-01 21:09:16 -0200

    Adicinado script de Click and Drag

---
+++ Scripts/ClickAndDrag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAndDrag : MonoBehaviour {
+
+    /*  Esse código permite a pessoa pegar um objeto com o mouse 
+     * e arrastá-lo pela cena.
+     */
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+    }
+
+}
