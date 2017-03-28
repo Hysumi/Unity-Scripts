00311e0644f5506be971cb15362c4ec8c76a8058
Henrique Sumitomo <Henrique Sumitomo>
2017-03-28 20:46:19 -0300

    Adicionando Script de rotação

---
+++ Scripts/SoftRotation_2DTopDown.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class SoftRotation_2DTopDown : MonoBehaviour {
+
+    public void SoftRotation(Vector2 direction, float rotationSpeed, GameObject g)
+    {
+        float ang = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        ang += 90;
+
+        if (ang < 0)
+            ang += 360;
+
+        float actualAngle = g.transform.localEulerAngles.z;
+        float difAngle = ang - actualAngle;
+
+        if (difAngle >= 0)
+        {
+            if (Mathf.Abs(difAngle) < 180)
+                RotationAnticlockwise(actualAngle, rotationSpeed, g);
+            else
+                RotationClockwise(actualAngle, rotationSpeed, g);
+        }
+        else
+        {
+            if (Mathf.Abs(difAngle) > 180)
+                RotationAnticlockwise(actualAngle, rotationSpeed, g);
+            else
+                RotationClockwise(actualAngle, rotationSpeed, g);
+        }
+    }
+
+    void RotationClockwise(float actual, float rotationSpeed, GameObject g)
+    {
+        g.transform.localEulerAngles =
+            new Vector3(0, 0, actual - rotationSpeed * Time.deltaTime);
+    }
+
+    void RotationAnticlockwise(float actual, float rotationSpeed, GameObject g)
+    {
+        g.transform.localEulerAngles =
+            new Vector3(0, 0, actual + rotationSpeed * Time.deltaTime);
+    }
+
+}
