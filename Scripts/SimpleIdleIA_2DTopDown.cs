99aaa45e6f1b3a2bc8786a0a06013b4601e9f7c2
Henrique Sumitomo <Henrique Sumitomo>
2017-03-28 20:33:39 -0300

    Adicionando IA Osciosa TopDown2D

---
+++ Scripts/SimpleIdleIA_2DTopDown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SimpleIdleIA_2DTopDown : MonoBehaviour {
+
+    Vector2 direction;
+    public float maxTime, velocity;
+    float actualTime, angle;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Timmer();
+        Idle();
+    }
+
+    void Idle()
+    {
+        if (actualTime == 0)
+            direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+
+        Vector3 newPos = direction * Time.deltaTime * velocity;
+        this.gameObject.transform.position -= newPos;
+    }
+
+    void Timmer()
+    {
+        if (maxTime < actualTime)
+            actualTime = 0;
+        else
+            actualTime += Time.deltaTime;
+    }
+}
