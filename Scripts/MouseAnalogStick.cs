1ae564855691104189007d83ac581fd3e97358f4
Henrique Sumitomo <Henrique Sumitomo>
2017-03-28 21:00:00 -0300

    Adicionando controle analógico com mouse

---
+++ Scripts/MouseAnalogStick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MouseAnalogStick : MonoBehaviour {
+
+    bool isDragging;
+    Vector2 dragOrigin;
+
+    Vector2 AnalogStick()
+    {
+        if (isDragging)
+        {
+            Vector2 dragVector;
+            Vector2 mousePosClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            dragVector = dragOrigin - mousePosClick;
+
+            Debug.DrawLine(dragOrigin, mousePosClick, Color.black);
+
+            if (Input.GetMouseButtonUp(0))
+                isDragging = false;
+            return (dragVector);
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                dragOrigin = ray.origin;
+            }
+        }
+        return (Vector2.zero);
+    }
+}
